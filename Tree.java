d5328db9314ce1ed494fdad28c087af1415b9512
tanducc111 <tannducc@gmail.com>
2024-04-03 19:16:32 +0700

    Update Tree.java
    them moi

--- Tree.java
+++ Tree.java
@@ -37,5 +37,32 @@ class MyBinaryTree {
     }
     int cao() {
         return cao(root);
+        int countNodes(TNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int leftCount = countNodes(root.left);
+            int rightCount = countNodes(root.right);
+
+            return 1 + leftCount + rightCount;
+        }
+
+        int countNodes() {
+            return countNodes(root);
+        }
+
+        int sumNodes(TNode root) {      
+            if (root == null) {
+                return 0;
+            }       
+            int leftSum = sumNodes(root.left);
+            int rightSum = sumNodes(root.right);
+            return root.data + leftSum + rightSum;
+        }   
+        int sumNodes() {
+            return sumNodes(root);
+        }
+
     }
 }
